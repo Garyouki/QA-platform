362eb440c8274f4294ce88d64a6ea699b6c41106
Gary-zzh <z.zihaogary@wustl.edu>
2022-12-11 00:22:18 -0600

    add server, config and extension to it

---
+++ server.py
@@ -0,0 +1,15 @@
+import imp
+from flask import Flask
+import config
+app = Flask(__name__)
+# bundle config file
+app.config.from_object(config)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello"
+
+
+if __name__ == '__main__':
+    app.run()
